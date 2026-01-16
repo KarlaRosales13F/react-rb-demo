3b56ca26f996a28865e71e5483c679cfbcc5444b
KarlaRosales13F <karla.rosalesc06@gmail.com>
2026-01-15 21:04:34 -0500

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import LabRbButtons from "./lab/LabRbButtons";
-import LabRbAlert from "./lab/LabRbAlert";
-import LabRbCard from "./lab/LabRbCard";
-import LabRbForm from "./lab/LabRbForm";
-import LabRbTable from "./lab/LabRbTable";
-import { useState } from "react";
+import './App.css'
+import RBHero from './components/rb/RBHero'
+import RBCarousel from './components/rb/RBCarousel'
+import RBCardGrid from './components/rb/RBCardGrid'
+import RBNewsletter from './components/rb/RBNewsletter'
 
-type LabKey = "buttons" | "alert" | "card" | "form" | "table";
-
-export default function App() {
-  const [lab, setLab] = useState<LabKey>("buttons");
+function App() {
 
   return (
-    <div>
-      <div style={{ padding: 16 }}>
-        <label style={{ display: "block", marginBottom: 6 }}>Selecciona LAB</label>
-        <select value={lab} onChange={(e) => setLab(e.target.value as LabKey)}>
-          <option value="buttons">Buttons</option>
-          <option value="alert">Alert</option>
-          <option value="card">Card</option>
-          <option value="form">Form</option>
-          <option value="table">Table</option>
-        </select>
-      </div>
-
-      {lab === "buttons" && <LabRbButtons />}
-      {lab === "alert" && <LabRbAlert />}
-      {lab === "card" && <LabRbCard />}
-      {lab === "form" && <LabRbForm />}
-      {lab === "table" && <LabRbTable />}
-    </div>
-  );
+    <>
+      <RBHero/>
+      <RBCarousel/>
+      <RBCardGrid/>
+      <RBNewsletter/>
+    </>
+  )
 }
+
+export default App
---
+++ src/components/pages/HomeRB.tsx
@@ -0,0 +1,15 @@
+import RBHero from "../rb/RBHero";
+import RBCarousel from "../rb/RBCarousel";
+import RBCardGrid from "../rb/RBCardGrid";
+import RBNewsletter from "../rb/RBNewsletter";
+
+export default function HomeRB() {
+  return (
+    <>
+      <RBHero />
+      <RBCarousel />
+      <RBCardGrid />
+      <RBNewsletter />
+    </>
+  );
+}
